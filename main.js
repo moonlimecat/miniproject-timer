d565d0debda57233808b52f824f551215c432b92
moonlimecat <limepunch212@gmail.com>
2024-08-06 21:34:38 -0400

    update

--- main.js
+++ main.js
@@ -8,28 +8,59 @@ let seconds = document.getElementById("seconds");
 
 let intervalId = null;
 
-function timer() {
-  if (minutes.value == 0 && seconds.value == 0) {
-    minutes.value = null;
-    seconds.value = null;
-  } else if (seconds.value > 0) {
-    seconds.value--;
-  } else if (minutes.value != 0 && seconds.value == 0) {
-    seconds.value = 59;
-    minutes.value--;
-  }
-  if (minutes.value < 10 || seconds.value < 10) {
-    minutes.value = minutes.value.toString().padStart("2", 0);
-    seconds.value = seconds.value.toString().padStart("2", 0);
-  }
-}
-
 function startTimer() {
   if (intervalId) return;
 }
 
 function stopTimer() {
   clearInterval(intervalId);
+}
+
+function inputLenFunc() {
+  if (minutes.value.length > 2 || seconds.value.length > 2) {
+    minutes.value = minutes.value.slice(0, 2);
+    seconds.value = seconds.value.slice(0, 2);
+  }
+}
+
+function nullValue() {
+  minutes.value = null;
+  seconds.value = null;
+}
+
+function minMaxNum() {
+  if (
+    minutes.value > 60 ||
+    minutes.value < 0 ||
+    seconds.value < 0 ||
+    seconds.value > 60
+  ) {
+    nullValue();
+    stopTimer();
+    startTimer();
+  }
+}
+
+function displayNum() {
+  if (minutes.value < 10 || seconds.value < 10) {
+    minutes.value = minutes.value.toString().padStart("2", 0);
+    seconds.value = seconds.value.toString().padStart("2", 0);
+  }
+}
+
+function timer() {
+  if (minutes.value == 0 && seconds.value == 0) {
+    stopTimer();
+    nullValue();
+  } else if (seconds.value > 0) {
+    seconds.value--;
+  } else if (minutes.value != 0 && seconds.value == 0) {
+    seconds.value = 59;
+    minutes.value--;
+  }
+  inputLenFunc();
+  minMaxNum();
+  displayNum();
 }
 
 start.addEventListener("click", function () {
@@ -43,8 +74,7 @@ pause.addEventListener("click", function () {
 });
 
 reset.addEventListener("click", function () {
-  minutes.value = null;
-  seconds.value = null;
+  nullValue();
   stopTimer();
   startTimer();
 });
